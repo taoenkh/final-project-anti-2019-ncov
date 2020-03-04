f40441788672bc9b72a05601289481cc86555f1f
Jiaqi Liu <jackie_liu@campus-021-230.ucdavis.edu>
2020-03-03 18:36:46 -0800

    added shard.js

---
+++ blockchain/Shard.js
@@ -0,0 +1,50 @@
+const Block = require('./block');
+
+class Shard{
+
+    constructor(shardnum){
+
+
+        this.shard = [Block.genesis(shardnum)]
+        this.shardnum = shardnum;
+    }
+
+    addBlock(data){
+
+        const block = Block.mineBlock(this.shard[this.shard.length-1],data, this.shardnum);
+
+        this.shard.push(block);
+
+        return block;
+
+    }
+
+    isValidChain(chain){
+        if(JSON.stringify(chain[0])!== JSON.stringify(Block.genesis())) return false;
+
+        for(let i=1; i<chain.length; i++)
+        {
+            const block = chain[i];
+            const lastBlock = chain[i-1];
+            if(block.lastHash !== lastBlock.hash || block.hash !== Block.blockHash(block)){
+                return false;
+
+            }
+
+        }
+        return true;
+    }
+    replaceChain(newChain){
+        if(newChain.length<= this.shard.length){
+            console.log('Recieved chain is not longer than the current chain');
+            return;
+        } else if(!this.isValidChain(newChain)){
+            console.log('the received chian is not valid.');
+            return;
+        }
+        console.log('Replacing blockchain with the new chain');
+        this.shard= newChain;
+
+    }
+}
+module.exports = Shard;
